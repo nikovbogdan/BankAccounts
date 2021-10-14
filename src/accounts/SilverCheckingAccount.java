6e44284763e8da1e3c6aecc443516d8d8370d37e
Bogdan Nikov <nikovbogdan@gmail.com>
2021-10-14 15:27:33 +0300

    SilverCheckingAccount created

---
+++ src/accounts/SilverCheckingAccount.java
@@ -0,0 +1,9 @@
+package accounts;
+
+public class SilverCheckingAccount extends CheckingAccount{
+    public SilverCheckingAccount(double balance, double interestRate, int rewardPoints) {
+        super(balance, interestRate, rewardPoints);
+    }
+
+
+}
